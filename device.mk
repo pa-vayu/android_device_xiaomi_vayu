433364dfc025348e2a64fd71df5379eafa327b27
Jake Weinstein <jake@aospa.co>
2023-10-07 14:56:51 +0900

    vayu: Disable custom content metadata region on <5.15 targets
    Pre-8550 devices do not currently support this.

--- device.mk
+++ device.mk
@@ -366,6 +366,9 @@ PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 4096
 # Disable UBWC-P on <6.1 targets
 TARGET_GRALLOC_HANDLE_HAS_NO_UBWCP := true
 
+# Disable custom content metadata region on <5.15 targets
+TARGET_GRALLOC_HANDLE_HAS_NO_CUSTOM_CONTENT_MD_RESERVED_SIZE := true
+
 # Overlays
 PRODUCT_PACKAGES += \
    CarrierConfigOverlayVayu \
