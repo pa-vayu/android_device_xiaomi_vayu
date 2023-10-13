d4c4378c7531e26d5f1fffce0d4958dbadbc018f
orges <orges@orgesified.com>
2023-10-13 16:22:41 +0000

    vayu: remove duplicates
     * exist in telephony component
    
    Signed-off-by: orges <orges@orgesified.com>

--- device.mk
+++ device.mk
@@ -402,10 +402,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.6.vendor \
-    android.hardware.radio.config@1.3.vendor \
-    android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.2.vendor \
     libjson.vendor \
     libprotobuf-cpp-full
 
