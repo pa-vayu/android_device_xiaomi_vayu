b1a2a0dd7f5d6fdeea9e6f60ba9be1f15bb4c5e1
orges <orges@orgesified.com>
2023-10-19 08:03:42 +0000

    vayu: build aidl drm service
    Signed-off-by: orges <orges@orgesified.com>

--- device.mk
+++ device.mk
@@ -195,8 +195,8 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey \
-    android.hardware.drm@1.3.vendor
+    android.hardware.drm-service.clearkey \
+    android.hardware.drm@1.4.vendor
 
 # Fastbootd
 PRODUCT_PACKAGES += \
